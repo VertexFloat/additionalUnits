80d740734e3f5419d3dac7cf66f0da5251d34005
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-02-27 20:44:29 +0100

    fixed automatic water supply bug (#20)

--- src/misc/PlaceableHusbandryWaterUnitExtension.lua
+++ src/misc/PlaceableHusbandryWaterUnitExtension.lua
@@ -1,6 +1,9 @@
 -- @author: 4c65736975, All Rights Reserved
--- @version: 1.0.0.0, 13|04|2023
+-- @version: 1.0.0.1, 02|05|2023
 -- @filename: PlaceableHusbandryWaterUnitExtension.lua
+
+-- Changelog (1.0.0.1):
+-- fixed issue where water fill level is still visible even when husbandry has automatic water supply
 
 PlaceableHusbandryWaterUnitExtension = {}
 
@@ -25,23 +28,25 @@ function PlaceableHusbandryWaterUnitExtension:initialize()
 		local infos = superFunc(husbandry)
 		local spec = husbandry.spec_husbandryWater
 
-		local info = {}
-		local fillType = self.fillTypeManager:getFillTypeByIndex(spec.fillType)
-		local capacity = husbandry:getHusbandryCapacity(spec.fillType)
-		local ratio = 0
+		if not spec.automaticWaterSupply then
+			local info = {}
+			local fillType = self.fillTypeManager:getFillTypeByIndex(spec.fillType)
+			local capacity = husbandry:getHusbandryCapacity(spec.fillType)
+			local ratio = 0
 
-		info.title = fillType.title
-		info.fillType = fillType.name
-		info.value = husbandry:getHusbandryFillLevel(spec.fillType)
+			info.title = fillType.title
+			info.fillType = fillType.name
+			info.value = husbandry:getHusbandryFillLevel(spec.fillType)
 
-		if capacity > 0 then
-			ratio = info.value / capacity
+			if capacity > 0 then
+				ratio = info.value / capacity
+			end
+
+			info.ratio = MathUtil.clamp(ratio, 0, 1)
+			info.invertedBar = true
+
+			table.insert(infos, info)
 		end
-
-		info.ratio = MathUtil.clamp(ratio, 0, 1)
-		info.invertedBar = true
-
-		table.insert(infos, info)
 
 		return infos
 	end)
@@ -50,11 +55,14 @@ function PlaceableHusbandryWaterUnitExtension:initialize()
 		superFunc(husbandry, infoTable)
 
 		local spec = husbandry.spec_husbandryWater
-		local fillLevel = husbandry:getHusbandryFillLevel(spec.fillType)
-		local formattedFillLevel, unit = self.additionalUnits:formatFillLevel(fillLevel, self.fillTypeManager:getFillTypeNameByIndex(spec.fillType), 0)
 
-		spec.info.text = formattedFillLevel .. ' ' .. unit
+		if not spec.automaticWaterSupply then
+			local fillLevel = husbandry:getHusbandryFillLevel(spec.fillType)
+			local formattedFillLevel, unit = self.additionalUnits:formatFillLevel(fillLevel, self.fillTypeManager:getFillTypeNameByIndex(spec.fillType), 0)
 
-		table.insert(infoTable, spec.info)
+			spec.info.text = formattedFillLevel .. ' ' .. unit
+
+			table.insert(infoTable, spec.info)
+		end
 	end)
 end
